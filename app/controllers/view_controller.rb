7cada7f64369ff68a93d75e6a76bd770c36fcf87
mponizil <misha.ponizil@gmail.com>
2014-04-06 20:11:03 -0400

    Show item feed with thumbnails

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -1,7 +1,6 @@
 class ViewController < UIViewController
   def viewDidLoad
     super
-    puts 'viewDidLoad'
   end
 
   def viewDidAppear(animated)
@@ -13,7 +12,41 @@ class ViewController < UIViewController
   end
 
   def showFeed
-    puts 'show feed now'
+    query = PFQuery.queryWithClassName('Item')
+    query.whereKey('collector', equalTo: PFUser.currentUser)
+    query.findObjectsInBackgroundWithBlock(lambda do |items, error|
+
+      @items = items
+
+      @dataTable = UITableView.alloc.initWithFrame(CGRectMake(0, 40, view.frame.size.width,
+        view.frame.size.height - 40), style: UITableViewStylePlain)
+      @dataTable.delegate = self
+      @dataTable.dataSource = self
+      @dataTable.backgroundView = nil
+      view.addSubview(@dataTable)
+
+    end)
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @items.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= 'cell'
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleValue1, reuseIdentifier: @reuseIdentifier)
+    end
+
+    item = @items[indexPath.row]
+    imageURL = NSURL.URLWithString(item['image'])
+    imageData = NSData.dataWithContentsOfURL(imageURL)
+    cell.imageView.setImage(UIImage.alloc.initWithData(imageData))
+    cell.textLabel.text = item['title']
+    cell.detailTextLabel.text = item['url']
+
+    cell
   end
 
   def showLogin
