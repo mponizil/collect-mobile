bbb4b01b3c19947ffd80d2ba008dcffdb99df48d
mponizil <misha.ponizil@gmail.com>
2014-04-14 02:00:53 -0400

    Fix logout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,23 +1,30 @@
 class MainViewController < ContainerViewController
+  def viewDidLoad
+    super
+
+    @authController = AuthController.alloc.init
+    @authController.delegate = self
+
+    @myCollectionController = MyCollectionController.alloc.init
+    @myCollectionController.delegate = self
+    @navigationController = UINavigationController.alloc.initWithRootViewController(@myCollectionController)
+  end
+
   def viewDidAppear(animated)
     if PFUser.currentUser
       showMyCollection
     else
-      @authController = AuthController.alloc.init
-      @authController.delegate = self
       addContentController(@authController)
     end
   end
 
   def showMyCollection
-    @myCollectionController = MyCollectionController.alloc.init
-    @myCollectionController.delegate = self
-    @navigationController = UINavigationController.alloc.initWithRootViewController(@myCollectionController)
     addContentController(@navigationController)
   end
 
   def myCollectionController(myCollectionController, didLogOutUser: user)
     removeContentController(@myCollectionController)
+    addContentController(@authController)
   end
 
   def authController(authController, didLogInUser: user)
